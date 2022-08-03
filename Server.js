576c8e5f039c2ec80300a9ab4b4aea69f860bed3
Ricardo-Senpai <94566332+AbilioGamaNJ@users.noreply.github.com>
2022-08-02 22:00:14 -0300

    modified:   Server.js 	modified:   package.json

--- Server.js
+++ Server.js
@@ -6,7 +6,10 @@ dotenv.config({path:'./config/config.env'})
 const port=process.env.port
 // Run Server
 const server=express()
-server.listen(port, console.log(`Servidor rodando na porta ${port}...`))
+
+server.listen(
+    port, console.log(`Servidor rodando no ambiente de ${process.env.NODE_ENV} na porta ${port}...`)
+    )
 
 server.get('/', (req,res) => res.send('Hello meu consagrado!'))
 
